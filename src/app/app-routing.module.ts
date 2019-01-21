8513c1845b6be48e06491a013f0256c940b2728c
Udhaya <udhayakumar.m@10decoders.in>
2019-01-21 14:56:27 +0530

    create project modal pop up

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
+
+const routes: Routes = [
+  { path: 'project', component: ProjectsComponent },
+  { path: '', redirectTo: 'project', pathMatch: 'full' },
+  { path: '**', redirectTo: 'project', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
